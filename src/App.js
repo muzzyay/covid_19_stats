c6376519c77a37b63bd67c730d77ad914c931fb0
muzzyay <muzzyay@gmail.com>
2020-03-14 00:36:49 -0700

    filtering added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     selected:null,
     world:null,
     stats: null,
+    filterValue: ''
   }
 
   componentWillMount(){
@@ -17,6 +18,18 @@ class App extends Component {
   }
 
   getWorldData = async ()=>{
+
+    // try{
+
+    //   const url = `https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/1/query?f=json&where=(Confirmed > 0) AND (Deaths>0) AND (Country_Region='China')&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Deaths desc,Country_Region asc,Province_State asc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true`
+
+    //   const response = await fetch(url);
+    //   const data = await response.json();
+    //   console.log(data, 'COVID')
+
+    // }catch(err){
+    //   console.log(err, "COVID ERR")
+    // }
 
     try{
       const response = await fetch("https://covid19.mathdro.id/api");
@@ -70,9 +83,11 @@ class App extends Component {
 
   
   render (){
-    const {selected, stats} = this.state;
+    const {selected, stats, filterValue} = this.state;
 
     if (!stats) return null;
+
+    let filteredCountries = (filterValue && Object.entries(countries).filter(([name, code])=> name.toLowerCase().includes(filterValue.toLowerCase()) || name.toLowerCase().includes(filterValue.toLowerCase()))) || Object.entries(countries);
     return (
       <>
       <Jumbotron fluid className="text-center">
@@ -92,14 +107,31 @@ class App extends Component {
           <Dropdown.Menu
           style={{height: "30vh", overflow: "auto"}}
           >
+            
+            <input 
+            type="text" 
+            placeholder="Type to Filter..." 
+            style={{border:'1px solid gray', borderRadius:"4px", padding: '3px 3px', marginLeft: '20px'}}
+            onChange={e=>this.setState({filterValue: e.target.value})}
+            value={filterValue}
+            />
+
+            <Dropdown.Divider/>
+            
+          {
+            !filterValue || "the world".includes(filterValue.toLowerCase())
+            ?
             <Dropdown.Item
                     key={'world'}
                    eventKey={'world'} 
                    onSelect={(country)=>this._handleSelect(null)}
                    active={!selected}
                    >THE WORLD</Dropdown.Item>
+            :
+            null
+          }
           {
-                Object.entries(countries).map(([country, code], index)=> (
+                filteredCountries.map(([country, code], index)=> (
                   <Dropdown.Item
                   key={index}
                    eventKey={country} 
